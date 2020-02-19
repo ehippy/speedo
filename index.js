fec3e80547f1a8b05ff3ec981c78b43b3c364a46
Patrick McDavid <pmcdavid@gmail.com>
2020-02-19 11:51:46 -0700

    packages

---
+++ index.js
@@ -0,0 +1,20 @@
+const si = require('systeminformation');
+ 
+// promises style - new since version 3
+// si.cpu()
+// .then(data => console.log(data))
+// .catch(error => console.error(error));
+
+setInterval(()=>{
+
+    si.cpuTemperature()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    
+    si.gpuTemperature()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    
+}, 1000)
+
+
